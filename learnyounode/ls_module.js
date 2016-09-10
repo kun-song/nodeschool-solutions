564f7144d39f922720a02078a1b12970fb962d00
satansk <satansk@hotmail.com>
2016-09-10 11:59:43 +0800

    模块化

--- learnyounode/ls_module.js
+++ learnyounode/ls_module.js
@@ -4,6 +4,9 @@
  * 知识点：
  * 1. 实现模块，只需要 module.exports = function(){} 将该函数暴漏给外界即可，使用该模块的代码，需要 requre('./XX'); 
  * 2. 与 require 普通模块相比，自定义的模块需要指定模块所在路径。
+ * 3. 导出模块的两种方式：（1）module.exports = function(){} （2）exports.ls = function(){}，在 ls_main.js 中，
+ * 分别需要用：const ls = require('./ls_module.js'); ls(); 
+ * 和 const module = require('./ls_module.js'); module.ls(); 的方式调用。
  * 
  * 注意：
  * 1. readdir 内部，错误和正常 两种情况需要由模块使用者提供的 callback 函数来处理，
@@ -29,4 +32,5 @@ function ls(file, ext, callback) {
 	});
 }
 
+// exports.ls = ls;
 module.exports = ls;
